72623104f4ed5d5175a161043cce50541a6c6e7d
cooukiez <cookiecenter@web.de>
2025-01-15 10:40:31 +0100

    remove unnecessary deps

--- run.sh
+++ run.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-RUNDB=false
 RUNSVELTE=false
 
 while getopts "ds" opt; do
   case $opt in
-    d)
-      RUNDB=true
-      ;;
 		s)
 			RUNSVELTE=true
 			;;
@@ -17,11 +13,6 @@ while getopts "ds" opt; do
       ;;
   esac
 done
-
-if [ "$RUNDB" = true ]; then
-	mongod --version
-	mongod --dbpath "./db" --port 27017
-fi
 
 if [ "$RUNSVELTE" = true ]; then
 	npm run dev -- --open
