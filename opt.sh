2c69027202a5df3fe34c38c309eae7a40dd37623
cooukiez <cookiecenter@web.de>
2025-01-20 09:59:42 +0100

    update objectschema, update run.sh to opt.sh, add database and migrations to .gitignore

---
+++ opt.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+INSTALL_DEPS=false
+GENERATE_PRISMA=false
+RESET_PRISMA=false
+MIGRATE_PRISMA=false
+RUN_SVELTE=false
+
+while getopts "igrms" opt; do
+  case $opt in
+    i)
+      INSTALL_DEPS=true
+      ;;
+    g)
+      GENERATE_PRISMA=true
+      ;;
+    r)
+      RESET_PRISMA=true
+      ;;
+    m)
+      MIGRATE_PRISMA=true
+      ;;
+		s)
+			RUN_SVELTE=true
+			;;
+
+    *)
+      echo "invalid argument"
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$INSTALL_DEPS" = true ]; then
+	npm i
+fi
+
+if [ "$GENERATE_PRISMA" = true ]; then
+	npx prisma generate
+fi
+
+if [ "$MIGRATE_PRISMA" = true ]; then
+  npx prisma migrate dev --name init
+fi
+
+if [ "$RESET_PRISMA" = true ]; then
+  npx prisma migrate reset
+fi
+
+if [ "$RUN_SVELTE" = true ]; then
+	npm run dev -- --open
+fi
