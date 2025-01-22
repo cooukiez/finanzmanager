13a48a5d07dde05a19b343b7f5d7b36f1ad10d82
HugoStink <Silschub1@web.de>
2025-01-22 10:45:17 +0100

    Commit Message

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -9,14 +9,13 @@
 	let { data, form }: { data: PageData, form: ActionData } = $props();
 </script>
 
-<div class="w-full">
-	<div class="flex justify-between items-center w-full">
-		<h2 class="text-center text-3xl font-bold tracking-tight text-gray-900">Admin View</h2>
+<div>
+	<div>
+		<h2>Admin View</h2>
 	</div>
 	<form
 			method="POST"
 			action="?/create"
-			class="w-full flex flex-col"
 
 			use:enhance={({ formElement }) => {
 			return async ({ result, update }) => {
@@ -30,54 +29,50 @@
 			};
 		}}
 	>
-		<div class="form-control w-full max-w-xs">
-			<label for="name" class="label">
-				<span class="label-text">Name</span>
+		<div>
+			<label for="name">
+				<span">Name</span>
 			</label>
 
 			<input
 					type="text"
 					name="name"
 					value={form?.name ?? ''}
-					class="input input-primary input-bordered w-full max-w-xs"
 			/>
 		</div>
-		<div class="form-control w-full max-w-xs">
-			<label for="email" class="label">
-				<span class="label-text">Email</span>
+		<div>
+			<label for="email">
+				<span>Email</span>
 			</label>
 			<input
 					type="email"
 					name="email"
 					value={form?.email ?? ''}
-					class="input input-primary input-bordered w-full max-w-xs"
 			/>
 		</div>
-		<div class="form-control w-full max-w-xs">
-			<label for="role" class="label">
-				<span class="label-text">Role</span>
+		<div>
+			<label for="role">
+				<span>Role</span>
 			</label>
 
 			<input
 					type="text"
 					name="role"
 					value={form?.role?? ''}
-					class="input input-primary input-bordered w-full max-w-xs"
 			/>
 		</div>
-		<div class="form-control w-full max-w-xs">
-			<label for="password" class="label">
-				<span class="label-text">Password</span>
+		<div>
+			<label for="password">
+				<span>Password</span>
 			</label>
 
 			<input
 					type="text"
 					name="password"
 					value={form?.password ?? ''}
-					class="input input-primary input-bordered w-full max-w-xs"
 			/>
 		</div>
-		<button class="mt-4 btn btn-primary w-full max-w-xs">Add</button>
+		<button>Add</button>
 		{#if form?.error}
 			<Alert message={form?.message} />
 		{/if}
