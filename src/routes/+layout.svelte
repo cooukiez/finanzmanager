e665cce2e756d992fecd3feafc52631506243f2a
cooukiez <cookiecenter@web.de>
2025-01-23 16:30:06 +0100

    fix navbar occluding content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -37,64 +37,62 @@
 
 </script>
 
-    <Navbar class="sticky top-0 z-20 border-b p-0" fluid={true}>
-        <NavBrand href="/">
-            {#if data.user}
-                <img src="favicon.png" class="p-1 me-3 h-9" alt="finanzmanager"/>
-                <span class="self-center whitespace-nowrap">Finanzmanager</span>
-            {:else}
-                <img src="favicon.png" class="p-1 me-3 h-9" alt="finanzmanager"/>
-                <span class="self-center whitespace-nowrap">Finanzmanager</span>
-            {/if}
-        </NavBrand>
-
+<Navbar class="sticky top-0 z-20 border-b p-0" fluid={true}>
+    <NavBrand href="/">
         {#if data.user}
-            <NavUl ulClass="flex flex-col md:flex-row p-2 mt-2 h-14 md:space-x-6 rtl:space-x-reverse md:mt-0 md:text-sm md:font-medium">
-                {#if data.user.role === "admin"}
-                    <NavLi href="/">Dashboard</NavLi>
-                {:else}
-                    <NavLi class="cursor-pointer justify-center items-center flex">
-                        Accounts
-                        <ChevronDownOutline class="inline"/>
-                    </NavLi>
-                    <Dropdown placement="bottom" class="w-44 z-20 shadow-lg">
-                        <DropdownItem href="/" class="w-full">Create new Account</DropdownItem>
-                    </Dropdown>
-
-                    <NavLi class="cursor-pointer justify-center items-center flex">
-                        Groups
-                        <ChevronDownOutline class="inline"/>
-                    </NavLi>
-                    <Dropdown placement="bottom" class="w-44 z-20 shadow-lg">
-                        <DropdownItem href="/" class="w-full">Join new Group</DropdownItem>
-                    </Dropdown>
-                {/if}
-
-                <div class="cursor-pointer justify-center items-center flex">
-                    <Avatar class="w-7 h-7" id="avatar-menu"/>
-                </div>
-                <div>
-                    <Dropdown placement="bottom" triggeredBy="#avatar-menu">
-                        <DropdownHeader>
-                            <span class="block truncate text-sm">{data.user.name}</span>
-                            <span class="block truncate text-sm">{data.user.email}</span>
-                        </DropdownHeader>
-                        <DropdownItem>Profile</DropdownItem>
-                        <DropdownItem>Settings</DropdownItem>
-                        <DropdownItem href="/lorem" class="w-full">Lorem ipsum</DropdownItem>
-                        <DropdownDivider/>
-                        <DropdownItem onclick={handleLogout}>Sign out</DropdownItem>
-                    </Dropdown>
-                </div>
-
-            </NavUl>
+            <img src="favicon.png" class="p-1 me-3 h-9" alt="finanzmanager"/>
+            <span class="self-center whitespace-nowrap">Finanzmanager</span>
         {:else}
-            <NavUl ulClass="flex p-2 mt-2 h-14 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:text-sm md:font-medium">
-                <NavLi href="/">Home</NavLi>
-            </NavUl>
+            <img src="favicon.png" class="p-1 me-3 h-9" alt="finanzmanager"/>
+            <span class="self-center whitespace-nowrap">Finanzmanager</span>
         {/if}
-    </Navbar>
-    <div class="overflow-scroll h-full">
-        {@render children()}
-    </div>
-</div>
+    </NavBrand>
+
+    {#if data.user}
+        <NavUl ulClass="flex flex-col md:flex-row p-2 mt-2 h-14 md:space-x-8 rtl:space-x-reverse md:mt-0 md:text-sm md:font-medium">
+            {#if data.user.role === "admin"}
+                <NavLi href="/">Dashboard</NavLi>
+            {:else}
+                <NavLi class="cursor-pointer justify-center items-center flex">
+                    Accounts
+                    <ChevronDownOutline class="inline"/>
+                </NavLi>
+                <Dropdown placement="bottom" class="w-44 z-20 shadow-lg">
+                    <DropdownItem href="/" class="w-full">Create new Account</DropdownItem>
+                </Dropdown>
+
+                <NavLi class="cursor-pointer justify-center items-center flex">
+                    Groups
+                    <ChevronDownOutline class="inline"/>
+                </NavLi>
+                <Dropdown placement="bottom" class="w-44 z-20 shadow-lg">
+                    <DropdownItem href="/" class="w-full">Join new Group</DropdownItem>
+                </Dropdown>
+            {/if}
+
+            <div class="cursor-pointer justify-center items-center flex">
+                <Avatar class="w-7 h-7" id="avatar-menu"/>
+            </div>
+            <div>
+                <Dropdown placement="bottom" triggeredBy="#avatar-menu">
+                    <DropdownHeader>
+                        <span class="block truncate text-sm">{data.user.name}</span>
+                        <span class="block truncate text-sm">{data.user.email}</span>
+                    </DropdownHeader>
+                    <DropdownItem>Profile</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownItem href="/lorem" class="w-full">Lorem ipsum</DropdownItem>
+                    <DropdownDivider/>
+                    <DropdownItem onclick={handleLogout}>Sign out</DropdownItem>
+                </Dropdown>
+            </div>
+
+        </NavUl>
+    {:else}
+        <NavUl ulClass="flex flex-col md:flex-row p-2 mt-2 h-14 md:space-x-8 rtl:space-x-reverse md:mt-0 md:text-sm md:font-medium">
+            <NavLi href="/">Home</NavLi>
+        </NavUl>
+    {/if}
+</Navbar>
+
+{@render children()}
