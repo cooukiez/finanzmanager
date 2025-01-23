a0d76883a3e5bf413bc94be76114689b9f462b31
cooukiez <cookiecenter@web.de>
2025-01-23 09:46:59 +0100

    update navbar styling but bugged

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -37,59 +37,61 @@
 
 </script>
 
-
-<Navbar class="">
-    <NavBrand href="/">
-        {#if data.user}
-            <span>Finanzmanager</span>
-        {:else}
-            <img src="favicon.png" class="me-3 h-6 sm:h-9" alt="finanzmanager"/>
-            <span class="self-center whitespace-nowrap">Finanzmanager</span>
-        {/if}
-
-    </NavBrand>
-
-    {#if data.user}
-        <div class="flex items-center md:order-2">
-            <Avatar class="w-8 h-8" id="avatar-menu"/>
-        </div>
-        <Dropdown placement="bottom" triggeredBy="#avatar-menu">
-            <DropdownHeader>
-                <span class="block truncate text-sm">{data.user.name}</span>
-                <span class="block truncate text-sm">{data.user.email}</span>
-            </DropdownHeader>
-            <DropdownItem>Profile</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-            <DropdownDivider/>
-            <DropdownItem onclick={handleLogout}>Sign out</DropdownItem>
-        </Dropdown>
-
-        <NavUl>
-            {#if data.user.role === "admin"}
-                <NavLi href="/">Dashboard</NavLi>
+<div class="relative">
+    <Navbar class="fixed w-full z-20 top-0 border-b block py-1">
+        <NavBrand href="/">
+            {#if data.user}
+                <img src="favicon.png" class="me-3 h-6 sm:h-9" alt="finanzmanager"/>
+                <span class="self-center whitespace-nowrap">Finanzmanager</span>
             {:else}
-                <NavLi class="cursor-pointer justify-center items-center flex">
-                    Accounts
-                    <ChevronDownOutline class="inline"/>
-                </NavLi>
-                <Dropdown  placement="bottom" class="w-44 z-20 shadow-lg">
-                    <DropdownItem href="/">Create new Account</DropdownItem>
-                </Dropdown>
-
-                <NavLi class="cursor-pointer justify-center items-center flex">
-                    Groups
-                    <ChevronDownOutline class="inline"/>
-                </NavLi>
-                <Dropdown  placement="bottom" class="w-44 z-20 shadow-lg">
-                    <DropdownItem href="/">Join new Group</DropdownItem>
-                </Dropdown>
+                <img src="favicon.png" class="me-3 h-6 sm:h-9" alt="finanzmanager"/>
+                <span class="self-center whitespace-nowrap">Finanzmanager</span>
             {/if}
-        </NavUl>
-    {:else}
-        <NavUl>
-            <NavLi href="/">Home</NavLi>
-        </NavUl>
-    {/if}
-</Navbar>
+        </NavBrand>
 
-{@render children()}
+        {#if data.user}
+            <NavUl>
+                {#if data.user.role === "admin"}
+                    <NavLi href="/">Dashboard</NavLi>
+                {:else}
+                    <NavLi class="cursor-pointer justify-center items-center flex">
+                        Accounts
+                        <ChevronDownOutline class="inline"/>
+                    </NavLi>
+                    <Dropdown placement="bottom" class="w-44 z-20 shadow-lg">
+                        <DropdownItem href="/">Create new Account</DropdownItem>
+                    </Dropdown>
+
+                    <NavLi class="cursor-pointer justify-center items-center flex">
+                        Groups
+                        <ChevronDownOutline class="inline"/>
+                    </NavLi>
+                    <Dropdown placement="bottom" class="w-44 z-20 shadow-lg">
+                        <DropdownItem href="/">Join new Group</DropdownItem>
+                    </Dropdown>
+                {/if}
+
+                <div class="flex items-center md:order-2">
+                    <Avatar class="w-8 h-8" id="avatar-menu"/>
+                </div>
+                <Dropdown placement="bottom" triggeredBy="#avatar-menu">
+                    <DropdownHeader>
+                        <span class="block truncate text-sm">{data.user.name}</span>
+                        <span class="block truncate text-sm">{data.user.email}</span>
+                    </DropdownHeader>
+                    <DropdownItem>Profile</DropdownItem>
+                    <DropdownItem>Settings</DropdownItem>
+                    <DropdownDivider/>
+                    <DropdownItem onclick={handleLogout}>Sign out</DropdownItem>
+                </Dropdown>
+            </NavUl>
+        {:else}
+            <NavUl>
+                <NavLi href="/">Home</NavLi>
+            </NavUl>
+        {/if}
+    </Navbar>
+    <div style="height:300px;" class="overflow-scroll pb-16">
+        {@render children()}
+    </div>
+</div>
