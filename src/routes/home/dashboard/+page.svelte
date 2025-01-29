78a2180568eecaced35b95abadea615489011887
HugoStink <Silschub1@web.de>
2025-01-29 08:24:58 +0100

    Commit Message

--- src/routes/home/dashboard/+page.svelte
+++ src/routes/home/dashboard/+page.svelte
@@ -7,14 +7,18 @@
     import type {PageData} from "./$types";
     import {sum} from "d3-array";
     import {format} from '@layerstack/utils';
+    import {redirect} from "@sveltejs/kit";
 
     let {data}: { data: PageData } = $props();
 
-    let accountData = data.accountData[0];
+    console.log(data.accountData);
+
+    let accountData = data.accountData;
 </script>
 
 <div class="flex flex-col gap-2">
     <div class="flex flex-row w-full gap-2">
+        {#each accountData as account}
         <Card.Root class="w-full">
             <Card.Header>
                 <Card.Title>Dein Ausgaben</Card.Title>
@@ -23,7 +27,7 @@
             <Card.Content>
                 <div class=" w-[800px] h-[300px] p-4 rounded">
                     <PieChart
-                            data={accountData}
+                            data={account}
 
                             key="expenditureType"
                             legend={{ placement: "top-left", orientation: "vertical" }}
@@ -41,7 +45,7 @@
                                     class="text-4xl"
                                     dy={4}
                                     textAnchor="middle"
-                                    value={format(sum(accountData, (d) => d.expenditureAmount)) +  "€"}
+                                    value={format(sum(account, (d) => d.expenditureAmount)) +  "€"}
                                     verticalAnchor="middle"
                             />
                             <Text
@@ -67,5 +71,6 @@
             </Card.Content>
 
         </Card.Root>
+            {/each}
     </div>
 </div>
