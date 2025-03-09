f271f15a53f5a531ddb2b43d066fdfe847070d54
Aphavalpha <Silschub1@web.de>
2025-03-09 22:47:37 +0100

    Commit Message

--- src/routes/home/accounts/schema.ts
+++ src/routes/home/accounts/schema.ts
@@ -6,7 +6,7 @@ export const accountCreateFormSchema = z.object({
   name: z
     .string()
     .min(3, { message: "Account name must contain at least 3 characters" })
-    .max(50, { message: "Account name must be less than 50 characters" }),
+    .max(40, { message: "Account name must be less than 50 characters" }),
   balance: z
     .number()
     .safe({ message: "Account balance must be a number" })
